4dbe89c8da3e6d275dad50426a6e1e001cf3bc6f
mandalay-rp <mandalay-rp@yandex.ru>
2020-10-01 12:01:30 +0300

    phone mask (js)

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -372,7 +372,7 @@ function inputs() {
       conditions = {
     phone: {
       format: {
-        pattern: /^\+7 \d{3}\ \d{3}\-\d{4}$|()/,
+        pattern: /^\+7\s\(?\d{3}\)?\s\d{3}\-\d{2}-?\d{2}$|()/,
         message: '^Некорректный номер телефона'
       },
       presence: {
@@ -423,7 +423,7 @@ function inputs() {
     }
   },
       mask = Inputmask({
-    mask: "+7 999 999-9999",
+    mask: "+7 (999) 999-99-99",
     showMaskOnHover: false,
     clearIncomplete: false
   }).mask(namspaces.phone); //validation events
